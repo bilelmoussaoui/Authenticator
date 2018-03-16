76f56bf631825953b64b9df3d8b4b3ea8025f00f
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2018-03-16 14:10:39 +0100

    more debug

--- Authenticator/widgets/accounts/list.py
+++ Authenticator/widgets/accounts/list.py
@@ -61,6 +61,7 @@ class AccountsList(Gtk.ListBox, GObject.GObject):
         accounts = Database.get_default().accounts
         for account in accounts:
             _id, name, secret_id, logo = account.values()
+            print(account.values())
             self.add(AccountRow(Account(_id, name, secret_id, logo)))
 
     def append(self, name, secret_id, logo):
